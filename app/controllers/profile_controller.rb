9b02999a62a4741c70bb099530b8e5bb75b6f169
Kacperek1337 <kacperek1337@protonmail.com>
2020-10-06 16:53:55 +0000

    use variants for profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,7 @@
 class ProfileController < ApplicationController
   def show
     @user = User.where('nickname ILIKE ?', params[:nickname]).first
+
+    request.variant = :not_found if @user.nil?
   end
 end
