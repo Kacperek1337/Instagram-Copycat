d72cc7421668532e6f18b6bd5562b80620f1037e
Kacperek1337 <kacperek1337@protonmail.com>
2020-10-11 17:29:14 +0200

    added post feed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,12 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    return request.variant = :not_found unless user_signed_in?
+
+    @posts = []
+    current_user.following.each do |fl|
+      @posts.concat fl.posts
+    end
+    @posts.sort_by!(&:created_at).reverse!
   end
 end
